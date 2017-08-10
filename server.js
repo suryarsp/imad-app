68a0dbf7ac0fcd7325bb36be887f3eab4f3bcd20
Surya Prakash <rspsuriya777@gmail.com>
2017-08-10 13:43:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var counter =0;
 app.get('/counter', function (req, res) {
-    var counter =0;
+    
     counter +=1;
     res.send(counter.toString() );
   
