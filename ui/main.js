e0b4b2dd562d63027a3e8cdc9fabe8b1a0941a5a
Surya Prakash <rspsuriya777@gmail.com>
2017-08-10 18:19:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -111,7 +111,49 @@ var rad=0;
         
     };
  
+ //to display cooments in each pages
  
+ var submit=document.getElementById('submit_comments');
+    submit.onclick=function(){
+      //Make request to server and send the names
+      
+      var request=new XMLHttpRequest();
+     
+     //Capture response and store it in var
+     request.onreadystatechange=function(){
+         
+         if(request.readyState===XMLHttpRequest.DONE)
+         {
+             //Take action on request
+             if(request.status===200)
+             //Received in network
+             {
+                 //Capture all names and render it as list
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list="";
+            for(var i=0;i<names.length;i++){
+                
+                list +='<li>'+names[i]+'<li>';
+                
+            }
+            //Converting list into unordered list
+            var ul=document.getElementById('nameList');
+            ul.innerHTML=list;
+                      }
+             
+          }
+          };
+            
+            
+            var nameinput=document.getElementById('name');
+            var name=nameinput.value;
+      
+          //Make the request
+          request.open('GET','http://rspsuriya777.imad.hasura-app.io/submit-name?name='+name,true);
+          request.send(null);
+        
+    };
  
  
  
