488a3da8964e062dbe07f47cb686582fd8e74cd6
Surya Prakash <rspsuriya777@gmail.com>
2017-08-10 12:40:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+//Working of changing HTML tags by js elements
+
+var html_Element=document.getElementById('foo');
+html_Element.innerHTML=
+`   <div>
+    <h2>Activities done</h2>
+    <div>
+    Finished Creating Profile page<hr>
+    Added CSS styles and more responsive<hr>
+    Made changes in server.js using Express js and handler functions<hr>
+    </span>
+    </div>
+    </div>
+`;
