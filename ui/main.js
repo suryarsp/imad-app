0d8067747139c9aa109baff2026d011ea6af49dd
Surya Prakash <rspsuriya777@gmail.com>
2017-08-10 16:28:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -80,7 +80,7 @@ var rad=0;
     var list="";
     for(var i=0;i<names.length;i++){
         
-        list= list +'<li>'+names[i]+'<li>';
+        list +='<li>'+names[i]+'<li>';
         
     }
     //Converting list into unordered list
