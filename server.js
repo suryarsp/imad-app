7c0e634b6e7dc5c8ef9683c2e19e0a4114e5643f
Surya Prakash <rspsuriya777@gmail.com>
2017-08-10 16:50:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/submit-name/:name', function (req, res) {
                                                         //We get query string grom url
                                                        // [/submit-name]?=xxxx-->Query String
 //Get the names from req
-var name=req.query.name;
+var name=req.params.name;
 
 names.push(name);
 //can't send array of strings as response
