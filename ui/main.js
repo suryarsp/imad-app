6350cdfd58dd9e742f43e4295d3a73a5e42de2eb
Surya Prakash <rspsuriya777@gmail.com>
2017-08-10 16:09:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -76,7 +76,7 @@ var rad=0;
       
       
       //Capture all names and render it as list
-    var names=['name1','name2','name3'];
+    var names=['name1','name2','name3','name4'];
     var list="";
     for(var i=0;i<names.length;i++){
         
